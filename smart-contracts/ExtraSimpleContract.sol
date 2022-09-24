baa76a1a0bed8794e2377d519c493474f031c2d6
deepak mishra <d.mishra@green-fusion.de>
2022-09-25 00:17:33 +0200

    inherited contract

--- smart-contracts/ExtraSimpleContract.sol
+++ smart-contracts/ExtraSimpleContract.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: None
+////SPDX-License-Identifier: None
 pragma solidity ^0.8.8;
 
 import './SimpleStorage.sol';
@@ -7,5 +7,9 @@ contract ExtraSimpleStorage is SimpleStorage {
 
         function store (uint256 favNo) public override {
             favNum = favNo + 49;
+        }
+
+        function extraFunc(string memory namePara) public pure returns (string memory) { 
+            return namePara;
         }
 }
