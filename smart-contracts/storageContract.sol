67078f151d742c7177b381ba12249d916ed3c9a9
deepak mishra <d.mishra@green-fusion.de>
2022-09-24 19:35:40 +0200

    simple contract change

--- smart-contracts/storageContract.sol
+++ smart-contracts/storageContract.sol
@@ -1,12 +1,26 @@
-//SPDX-License-Identifier: MIT
+//SPD//SPDX-License-Identifier: None
 pragma solidity ^0.8.8;
 
-import "./contractOne.sol";
+import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    FirstSmart public importContract;
+    SimpleStorage[] public simpleStorageArray;
 
-    function deployingContra() public {
-        importContract = new FirstSmart();
+    function deployContracts() public {
+        SimpleStorage simpleContractOne = new SimpleStorage();
+        simpleStorageArray.push(simpleContractOne);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
