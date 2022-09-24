820c43473dd7a31f487564b87724a02caf078091
deepak mishra <d.mishra@green-fusion.de>
2022-09-24 19:35:20 +0200

    simple contract change

--- smart-contracts/SimpleStorage.sol
+++ smart-contracts/SimpleStorage.sol
@@ -1,29 +1,33 @@
-//SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: None
 pragma solidity ^0.8.8;
 
 contract SimpleStorage {
-    uint256 public varOne = 5;
+    uint256 public favNum = 5;
     
     //array,struct,mapping
     struct People {
         uint256 favouriteNumber;
         string name;
     }
-    //People public krshn = People({favouriteNumber:11, name: "vasu"});
-    function  store(uint256 ff) public {
-        varOne = ff;
+    //People public krshn  = People({favouriteNumber:11, name: "vasu"});
+    
+    People[] public person;
+
+     //hash table for number assigned to name
+    mapping(uint256 => string) public nameTofavNum;
+
+    function  store(uint256 fnum) public {
+        favNum = fnum;
     }
 
-    People[] public person;
-    mapping(uint256 => string) public favNum;
     //calldata,memory,storage
     function addPerson(uint256  _favouriteNumber, string memory _name) public {
           //man memory firstMan = man({age:_age, name:_name});
           person.push(People(_favouriteNumber,_name));
-          favNum[_favouriteNumber] = _name;
+          nameTofavNum[_favouriteNumber] = _name;
     }
-     function  retrieve() public view returns(uint256) {
-        //varOne = ff;
-        return varOne;
+
+    function  retrieve() public view returns(uint256) {
+        return favNum;
     }
 }
