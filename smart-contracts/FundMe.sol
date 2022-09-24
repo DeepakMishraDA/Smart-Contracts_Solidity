9415cb7ae1a06ccae7554a87e00e354ae5202ba1
deepak mishra <d.mishra@green-fusion.de>
2022-09-25 00:26:39 +0200

    fund transfer function createde

---
+++ smart-contracts/FundMe.sol
@@ -0,0 +1,10 @@
+//getting, withdrawing and setting a minimum value of funds taken from users!
+
+//SPDX-License-Identifier: None
+pragma solidity ^0.8.8;
+contract FundMe {
+
+    function fund() public payable {}
+
+    function withdraw() public {}
+}
